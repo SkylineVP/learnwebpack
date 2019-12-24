a8396b0e0e0bdd7c0529debcdb1a490afda761dc
SkylineVP <vladpolizp@gmail.com>
2019-12-24 15:34:55 +0200

    Import css style and img on webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import _ from 'lodash';
-
+import styles from './style.css';
+import Icon from './img.jpeg';
 function component() {
     const element = document.createElement('div');
 
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+    element.classList.add(styles.hello);
+    const img=document.createElement("img");
+    img.src=Icon;
+    element.appendChild(img);
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,30 @@
 const path=require("path");
-module.exports={
+module.exports= {
   entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname,"dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {loader: 'css-loader',
+            options: {
+            importLoaders:1,
+              modules:true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpeg|gif)$/,
+        use:[
+            "file-loader",
+        ],
+      },
+    ]
   }
 };
